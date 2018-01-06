a0e96b860d43c2d6013f6aaf9a3df5f26410c7a2
Twinparadox <nww731@gmail.com>
2018-01-06 12:17:52 +0900

    Add Layout

--- BluetoothChat/BluetoothChat/Resources/Resource.Designer.cs
+++ BluetoothChat/BluetoothChat/Resources/Resource.Designer.cs
@@ -73,10 +73,19 @@ namespace BluetoothChat
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Main = 2130837504;
+			public const int device_list = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int title = 2130837505;
+			public const int device_name = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Main = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int message = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int title = 2130837508;
 			
 			static Layout()
 			{
